4a65eb9775045b6f9dbcca0f3ee2725a5ec98841
Vito045 <vito045@icloud.com>
2019-12-11 23:40:06 +0200

    first commit

---
+++ src/hompage.component.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './homepage.style.scss'
+
+export const HomePage = () => (
+  <div className="homepage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NAME</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NAME</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NAME</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NAME</span>
+        </div>
+      </div>
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NAME</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
